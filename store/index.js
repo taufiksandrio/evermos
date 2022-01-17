2cdf57485c8fb2171d0ec2fdcb617d5fdd4a46dd
taufiksandrio <taufik.hidayatullah091994@gmail.com>
2022-01-17 17:36:39 +0700

    third commit

--- store/index.js
+++ store/index.js
@@ -20,13 +20,13 @@ export const mutations = {
 
 export const actions = {
     async getProducts ({commit}) {
-        axios.get('http://my-json-server.typicode.com/taufiksandrio/products/products')
+        axios.get('https://my-json-server.typicode.com/taufiksandrio/products/products')
         .then(response => {
             commit('setProduct', response.data)
         })
     },
     async getDetail ({commit}, data) {
-        axios.get('http://my-json-server.typicode.com/taufiksandrio/products/products?slug='+ data)
+        axios.get('https://my-json-server.typicode.com/taufiksandrio/products/products?slug='+ data)
         .then(response => {
             commit('setDetail', response.data[0])
         })
